0db70c33eabd7edc15f3da4e3c33891bcb50e39b
Drake <148833506+delliott18@users.noreply.github.com>
2025-04-06 20:14:02 -0700

    Updated main.lua
    v 1.0.1 changes

--- main.lua
+++ main.lua
@@ -1,39 +1,51 @@
-SMODS.Atlas {
-    key = 'Modded_Jokers',
-    path = 'Jokers.png',
-    px = 71,
-    py = 95
-}
+G.MoreBalatro = {}
+G.MoreBalatro.C = {}
+G.MoreBalatro.FUNCS = {}
+G.MoreBalatro.config = SMODS.current_mod.config
 
-SMODS.Joker {
-    key = 'crib_joker',
-    loc_txt = {
-        name = 'The Crib',
-        text = {
-            "{X:mult,C:white} X#1# {} Mult if played",
-            "hand contains",
-            "a {C:attention}Full House{}"
-        }
-    },
-    blueprint_compat = true,
-    perishable_compat = true,
-    eternal_compat = true,
-    rarity = 3,
-    atlas = "Modded_Jokers",
-    pos = { x = 0, y = 0 },
-    cost = 10,
-    unlocked = true,
-    discovered = true,
-    config = { extra = { Xmult = 3 } },
-    loc_vars = function(self, info_queue, card)
-		return { vars = { card.ability.extra.Xmult } }
-	end,
-    calculate = function(self, card, context)
-        if context.joker_main and next(context.poker_hands["Full House"]) then
-            return {
-                message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult } },
-				Xmult_mod = card.ability.extra.Xmult
-            }
-        end
-    end
-}
+if G.MoreBalatro.config.jokers_enabled then  SMODS.load_file('objects/jokers.lua')() end
+
+SMODS.Atlas({
+    key = 'modicon',
+    path = 'modicon.png',
+    px = '34',
+    py = '34'
+})
+
+SMODS.current_mod.config_tab = function()
+	return {
+	  n = G.UIT.ROOT,
+	  config = { align = 'cm', padding = 0.07, emboss = 0.05, r = 0.1, colour = G.C.BLACK, minh = 4.5 ,minw = 7 },
+	  nodes = {
+		{
+			n = G.UIT.R,
+			config = { align = 'cm'},
+			nodes = {
+				{ n = G.UIT.T, config = { text = localize('MoreBalatro_feature_enable'), colour = G.C.WHITE, scale = 0.5 }},
+			}
+		},
+		{
+			n = G.UIT.R,
+			config = { align = 'cm', minh = 0.6 },
+			nodes = {
+				{ n = G.UIT.T, config = { text = localize('MoreBalatro_requires_restart'), colour = G.C.RED, scale = 0.4 }},
+			}
+		},
+		{
+		  n = G.UIT.R,
+		  nodes = {
+			{
+			  n = G.UIT.C,
+			  nodes = {
+				create_toggle {
+				  label = localize('MoreBalatro_jokers_enabled'),
+				  ref_table = G.MoreBalatro.config,
+				  ref_value = 'jokers_enabled',
+				},
+			  }
+			},
+		  }
+		}
+	  }	
+	}
+  end
